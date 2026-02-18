eb8a7f1d0dcbe9ff77d0c0aee292a5f7ca106562
leela-vigneswar <vigneshlevis@gmail.com>
2026-02-18 22:29:17 +0530

    shell

--- shell practice/03-variables.sh
+++ shell practice/03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Trump # no space between value and =
 PERSON2=Putin
 
-echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
-echo "$PERSON1:: doing fine. What are you learning $PERSON2?"
-echo "$PERSON2:: I am learning Shell. What about you?"
+echo "$PERSON1:: Hello $PERSON2, when is your war with Ukraine going to end?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. I am not sure when it is going to end, but I hope it ends soon."
+echo "$PERSON1:: Iam planning to invade venezulla, this might trouble you Mr.$PERSON2?"
+echo "$PERSON2:: I am not sure about that, but I will be ready for it if it happens."
